7b32fbf25a8dcd08fc90dfa9687cd43e8dccb52c
Garrett <90536997+garrett-huggins@users.noreply.github.com>
2023-12-06 15:00:42 -0500

    add pages view

--- src/components/deckPreviewContainer.tsx
+++ src/components/deckPreviewContainer.tsx
@@ -23,10 +23,19 @@ interface DeckPreviewContainerProps {
   }[];
 }
 
+const PAGE_SIZE = 12;
+
 export default function DeckPreviewContainer({
   decks,
 }: DeckPreviewContainerProps) {
   const [showEdit, setShowEdit] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginatedDecks = decks.slice(
+    (currentPage - 1) * PAGE_SIZE,
+    currentPage * PAGE_SIZE
+  );
+
   return (
     <div>
       <div className="mb-4 flex justify-center space-x-4">
@@ -40,9 +49,30 @@ export default function DeckPreviewContainer({
         </Button>
       </div>
       <div className="grid lg:grid-cols-4 md:grid-cols-3 gap-3">
-        {decks?.map((deck) => (
+        {paginatedDecks?.map((deck) => (
           <DeckPreview key={deck.id} deck={deck} showEdit={showEdit} />
         ))}
+      </div>
+      <div className="text-center mt-4">
+        <p>Page</p>
+        <div className="flex justify-center mt-2">
+          {[...Array(Math.ceil(decks.length / PAGE_SIZE)).keys()].map(
+            (page) => (
+              <Button
+                key={page + 1}
+                onClick={() => setCurrentPage(page + 1)}
+                variant="secondary"
+                className={`mx-2 px-4 py-2 bg-secondary ${
+                  currentPage === page + 1
+                    ? "bg-primary text-primary-foreground"
+                    : "hover:bg-primary"
+                }`}
+              >
+                {page + 1}
+              </Button>
+            )
+          )}
+        </div>
       </div>
     </div>
   );
