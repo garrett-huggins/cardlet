55ebe73785a8e9d10a88fed381ac0fcf73236f34
Garrett <90536997+garrett-huggins@users.noreply.github.com>
2023-12-06 02:52:50 -0500

    add option to delete decks

--- src/components/deckPreview.tsx
+++ src/components/deckPreview.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Card,
   CardContent,
@@ -5,6 +6,10 @@ import {
   CardHeader,
 } from "@/components/ui/card";
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { Trash2 } from "lucide-react";
+import { supabase } from "@/utils/supabase";
+import { useAuth } from "@clerk/nextjs";
 
 interface DeckPreviewProps {
   id: string;
@@ -21,11 +26,40 @@ interface DeckPreviewProps {
   }[];
 }
 
+// remove deck from supabase
+const removeDeck = async (id: string, userId: string) => {
+  // const supabase = await supabaseAuth(supabaseAccessToken);
+  // delete deck base on id
+  const { data, error } = await supabase
+    .from("decks")
+    .delete()
+    .match({ id: id, user_id: userId });
+  if (error) {
+    console.error(error);
+  } else {
+    window.location.reload();
+  }
+};
+
 export default function DeckPreview({ deck }: { deck: DeckPreviewProps }) {
   const link = `/decks/${deck.id}`;
+  const { userId } = useAuth();
+  // const test = async () => {
+  //   const supabaseAccessToken = await getToken({ template: "supabase" });
+  //   console.log("supabase", supabaseAccessToken);
+  // };
+  // test();
   return (
-    <Link href={link}>
-      <Card className="w-full min-h-[200px] relative dark:hover:bg-primary-foreground hover:bg-primary-foreground">
+    <Card className="w-full min-h-[200px] relative dark:hover:bg-primary-foreground hover:bg-primary-foreground">
+      <Button
+        //  @ts-ignore
+        onClick={() => removeDeck(deck.id, userId)}
+        variant="destructive"
+        className="absolute px-2 top-0 right-0 m-2 opacity-80"
+      >
+        <Trash2 />
+      </Button>
+      <Link href={link}>
         <CardHeader>
           <p className="text-lg font-semibold">{deck.name}</p>
         </CardHeader>
@@ -33,7 +67,7 @@ export default function DeckPreview({ deck }: { deck: DeckPreviewProps }) {
         <CardFooter className="absolute bottom-0 w-full">
           <p className="text-muted-foreground">{deck.cards.length} cards</p>
         </CardFooter>
-      </Card>
-    </Link>
+      </Link>
+    </Card>
   );
 }
